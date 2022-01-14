c30e8a0a451d2a79ae7defd005855970bc4fcf1d
yichen1984 <yichenue@gmail.com>
2022-01-14 03:45:59 +0000

    handsome add

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <div class="container-fluid">
+     <ul class="nav nav-tabs">
+       <li class="nav-item">
+         <button class="nav-link active" aria-current="page">Recipes</button>
+       </li>
+       <li class="nav-item">
+         <button class="nav-link">Add New</button>
+       </li>
+     </ul>
+   </div>
+ </div>
+</template>
+<script>
+
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
